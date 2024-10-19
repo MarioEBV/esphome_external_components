adf54a535799eccb171e9f27d05860a7cb255adb
MarioEBV <mario.vermeir@ebv.com>
2024-10-19 13:27:16 +0200

    Update rrh62000.h

--- components/RRH62000/rrh62000.h
+++ components/RRH62000/rrh62000.h
@@ -14,7 +14,7 @@ class RRH62000Sensor : public PollingComponent, public uart::UARTDevice {
   void set_eco2_sensor(sensor::Sensor *eco2_sensor) { eco2_sensor_ = eco2_sensor; }
   void set_tvoc_sensor(sensor::Sensor *tvoc_sensor) { tvoc_sensor_ = tvoc_sensor; }
   void set_pm_1_0_kci_sensor(sensor::Sensor *pm_1_0_kci_sensor) { pm_1_0_kci_sensor_ = pm_1_0_kci_sensor; }
-  void set_pm_2_5_kci_sensor(sensor::Sensor *pm_2_5__kcisensor) { pm_2_5_kci_sensor_ = pm_2_5_kci_sensor; }
+  void set_pm_2_5_kci_sensor(sensor::Sensor *pm_2_5_kci_sensor) { pm_2_5_kci_sensor_ = pm_2_5_kci_sensor; }
   void set_pm_10_0_kci_sensor(sensor::Sensor *pm_10_0_kci_sensor) { pm_10_0_kci_sensor_ = pm_10_0_kci_sensor; }
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
   void set_humidity_sensor(sensor::Sensor *humidity_sensor) { humidity_sensor_ = humidity_sensor; }
